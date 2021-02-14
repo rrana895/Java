865f4f95bfb548fb47dbb4e47b40ccdc784969e6
rrana895 <60363480+rrana895@users.noreply.github.com>
2021-02-13 23:39:53 -0500

    Add files via upload

---
+++ Strings/Str_Easy1/JavaSubstring.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String str="";
+        for(int i=start;i<end;i++)
+        {
+        System.out.print(S.charAt(i));
+        }
+    }
+}
+
